ff4afa7bd0d70249f656350b20b9cda53e6c4100
mehdi <mehdighor23@gmail.com>
2020-10-18 18:51:30 +0330

    Setup webpack for css style extractor

--- src/component/AddExpense.js
+++ src/component/AddExpense.js
@@ -4,22 +4,22 @@ import { connect } from "react-redux";
 import { addExpense } from "../actions/expenses";
 
 export class AddExpense extends React.Component {
-  onExpenseFromSubmit = (expense) => {
-    this.props.addExpense(expense);
-    this.props.history.push("/");
-  };
+    onExpenseFromSubmit = (expense) => {
+        this.props.addExpense(expense);
+        this.props.history.push("/");
+    };
 
-  render() {
-    return (
-      <div>
-        <h5>Add New Expense</h5>
-        <ExpenseFrom onSubmit={this.onExpenseFromSubmit} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h5>Add New Expense</h5>
+                <ExpenseFrom onSubmit={this.onExpenseFromSubmit} />
+            </div>
+        );
+    }
 }
 const mapDispatchToProps = (dispatch) => ({
-  addExpense: (expense) => dispatch(addExpense(expense)),
+    addExpense: (expense) => dispatch(addExpense(expense)),
 });
 
 export default connect(undefined, mapDispatchToProps)(AddExpense);
