1e5badcf67853ec90d0b4e461a4e0e09edf01675
mehdi <mehdighor23@gmail.com>
2020-11-14 16:08:22 +0330

    fix server file

--- server/server.js
+++ server/server.js
@@ -1,18 +1,15 @@
 const path = require('path');
-let express = require('express')
-let app = express();
-const publicPath = path.join(__dirname , '..' , 'public' );
-const port = process.env.PORT ||  3000;
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.get('*' , (req ,res) => {
-    res.sendFile(path.join(publicPath,'index.html'));
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
 
-const server =app.listen(port ,"127.0.0.1" , () => {
-
-    console.log('Express server http://' + server.address().address+":" +server.address().port);
-
-
+app.listen(port, () => {
+  console.log('Server is up!');
 });
